bd519dc464c60191cf82e71b936d9990b650313c
Mike Smith <grimbough@gmail.com>
2019-12-17 13:24:31 +0100

    slurm

--- slurm-submission.sh
+++ slurm-submission.sh
@@ -3,6 +3,7 @@
 #SBATCH --mem=24G
 #SBATCH --time=0-10:0
 #SBATCH --output=osca-builder.log
+#SBATCH --error=osca-builder.err
 
-singularity exec -B /g/huber/users/msmith/R-lib/R-4.0_BioC-3.11_OSCA/:/usr/local/lib/R/host-site-library/ osca-singularity.simg R make build
+singularity exec -B /g/huber/users/msmith/R-lib/R-4.0_BioC-3.11_OSCA/:/usr/local/lib/R/host-site-library/ osca-singularity.simg make build
 
