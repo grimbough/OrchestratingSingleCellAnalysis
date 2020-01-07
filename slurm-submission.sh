fdc1488d8c32fc6f8ec1303a163f3cccb491b299
Mike Smith <grimbough@gmail.com>
2020-01-07 23:53:33 +0100

    update submission script

--- slurm-submission.sh
+++ slurm-submission.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=osca-builder
+#SBATCH --nodes=1
 #SBATCH --mem=24G
-#SBATCH --time=0-10:0
-#SBATCH --output=osca-builder.log
-#SBATCH --error=osca-builder.err
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=1-0:0
+#SBATCH --output=/g/huber/users/msmith/OrchestratingSingleCellAnalysis/logs/osca-builder.log
 
-singularity exec -B /g/huber/users/msmith/R-lib/R-4.0_BioC-3.11_OSCA/:/usr/local/lib/R/host-site-library/ osca-singularity.simg make build
+PROJ_ROOT=/g/huber/users/msmith/OrchestratingSingleCellAnalysis/
+R_LIB=/g/huber/users/msmith/R-lib/R-4.0_BioC-3.11_OSCA/
+
+cd $PROJ_ROOT
+
+srun singularity exec -B $R_LIB:/usr/local/lib/R/host-site-library/ -B /scratch $PROJ_ROOT/osca-singularity.simg R -e 'BiocManager::install(update = TRUE, ask = FALSE)'
+srun singularity exec -B $R_LIB:/usr/local/lib/R/host-site-library/ -B /scratch $PROJ_ROOT/osca-singularity.simg R -e 'OSCAUtils::spawnBook("/scratch/msmith/test2"); OSCAUtils::compileWorkflows("/scratch/msmith/test2", fresh=FALSE)'
+srun singularity exec -B $R_LIB:/usr/local/lib/R/host-site-library/ -B /scratch $PROJ_ROOT/osca-singularity.simg R -e 'setwd("/scratch/msmith/test2"); bookdown::render_book("index.Rmd", "bookdown::gitbook", quiet = FALSE, output_dir = "docs", new_session = TRUE)'
+
 
